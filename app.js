61b05dc14feed8e5a8ab83e47d0ce197567e4e58
exert777 <alter0910@mail.ru>
2023-07-18 18:59:17 +0300

    second commit

--- app.js
+++ app.js
@@ -3,34 +3,65 @@
 const input = document.querySelector(".form-control");
 const addBtn = document.querySelector("#create");
 const listElement = document.querySelector(".list-group");
-const notes = ["Изучить js", "Поесть"];
+const notes = [
+  {
+    title: "Изучить js",
+    completed: true
+  }, 
+  {
+    title:"Поесть",
+    completed: false
+  },
+];
 
-function render() {
-  notes.forEach((element) => {
-    listElement.insertAdjacentHTML("afterbegin", getTemplate(element));
-  });
-}
+function getTemplate(notes) {
 
-function getTemplate(title) {
+  /*Применение ТЕРНАРНОГО ВЫРАЖЕНИЯ для быстрой проверка свойства competed у объекта заметки, 
+  если true надпись зачеркивается  */
   return `
     <li
         class="list-group-item d-flex justify-content-between align-items-center"
     >
-        <span>${title}</span>
+        <span class = ${notes.completed ? 'text-decoration-line-through' : ''}>${notes.title}</span>
         <span>
-        <span class="btn btn-small btn-success">&check;</span>
+        <span class="btn btn-small btn-${notes.completed ? 'warning' : 'success'}">&check;</span>
         <span class="btn btn-small btn-danger">&times;</span>
         </span>
     </li>`;
 }
 
+function render() {
+  /* notes.forEach((element) => {
+    listElement.insertAdjacentHTML("afterbegin", getTemplate(element));
+  }); */
+  for (let [index, note] of notes.entries()){
+    note.id = index;
+    listElement.insertAdjacentHTML("afterbegin", getTemplate(note));
+  };
+};
+
+
+
 addBtn.addEventListener("click", () => {
   if (input.value === "") {
     return;
   } else {
-    listElement.insertAdjacentHTML("afterbegin", getTemplate(input.value));
+
+    const newNote = {
+        title:`${input.value}`,
+        completed: true
+    } 
+
+    listElement.insertAdjacentHTML("afterbegin", getTemplate(newNote));
+
+    notes.push(newNote);
+
+    /* render(); */
   }
   input.value = "";
 });
 
 render();
+
+
+/* Сделать добавление индекса каждому новому объекту */
