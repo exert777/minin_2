27b92c0a09609c317f98bd89215a4c2c980d0952
exert777 <alter0910@mail.ru>
2023-07-20 18:20:17 +0300

    3 commit

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const notes = [
   },
 ];
 
-function getTemplate(notes) {
 
+
+function getTemplate(notes) {
   /*Применение ТЕРНАРНОГО ВЫРАЖЕНИЯ для быстрой проверка свойства competed у объекта заметки, 
   если true надпись зачеркивается  */
   return `
@@ -24,44 +25,59 @@ function getTemplate(notes) {
     >
         <span class = ${notes.completed ? 'text-decoration-line-through' : ''}>${notes.title}</span>
         <span>
-        <span class="btn btn-small btn-${notes.completed ? 'warning' : 'success'}">&check;</span>
-        <span class="btn btn-small btn-danger">&times;</span>
+        <span class="btn btn-small btn-${notes.completed ? 'warning' : 'success'}"  data-type = 'toggle' data-index = '${notes.id}'>&check;</span>
+        <span class="btn btn-small btn-danger"  data-type = 'remove' data-index = '${notes.id}'>&times;</span>
         </span>
     </li>`;
+}
+/* Функция создает новый объект "заметку" определяет его индекс в массиве заметок,
+записывает индекс в соответствующее свойство объекта и возвращает объект */
+function createNewNote (input) {
+  const newNote = {
+      title:`${input.value}`,
+      completed: true
+  };
+  notes.push(newNote);
+  newNote.id = notes.indexOf(newNote)
+  return newNote;
 }
 
 function render() {
   /* notes.forEach((element) => {
     listElement.insertAdjacentHTML("afterbegin", getTemplate(element));
   }); */
+  listElement.innerHTML = '';
   for (let [index, note] of notes.entries()){
     note.id = index;
     listElement.insertAdjacentHTML("afterbegin", getTemplate(note));
   };
 };
 
+/* Вешаем обработчик события клик на весь список */
+listElement.addEventListener('click', event => {
+ /*если у элемента накотором сработало событие есть индекс то выполняются дальнейшие инструкции */
+  if(event.target.dataset.index){
+    const index = Number(event.target.dataset.index);
+    const type = event.target.dataset.type;
+    /* если тип элемента toggle, то значение свойства объекта массива с индеком такого элемента 
+    меняется на противоположное */
+    if(type === 'toggle'){
+      notes[index].completed = !notes[index].completed
+    }
+  }
+  render();
+}) 
 
 
 addBtn.addEventListener("click", () => {
   if (input.value === "") {
     return;
   } else {
-
-    const newNote = {
-        title:`${input.value}`,
-        completed: true
-    } 
-
-    listElement.insertAdjacentHTML("afterbegin", getTemplate(newNote));
-
-    notes.push(newNote);
-
-    /* render(); */
+    listElement.insertAdjacentHTML("afterbegin", getTemplate(createNewNote(input)));
   }
   input.value = "";
 });
 
 render();
 
-
-/* Сделать добавление индекса каждому новому объекту */
+/* Доделать функционал кнопок */
